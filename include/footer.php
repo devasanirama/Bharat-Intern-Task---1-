d20409701c16a5ea26b9eef6dc9afab8ee2cd4bd
Devasani Ramakrishna <135047614+devasanirama@users.noreply.github.com>
2024-01-13 17:43:46 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,2 @@
+<div class="pull-right hidden-xs"> <b>Version</b> 1.0 </div>
+<strong>Copyright &copy; 2023 <a href="http://www.campcodes.com" target="_blank">Mello X Bytezz</a>.</strong> All rights reserved.
